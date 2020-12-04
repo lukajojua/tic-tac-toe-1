e9a1d378b239fc4e88853de5f76b55fedfbaada2
lukajojua <73716112+lukajojua@users.noreply.github.com>
2020-12-05 02:00:54 +0400

    first commit

--- app/src/main/java/ge/msda/myapplication/MainActivity.kt
+++ app/src/main/java/ge/msda/myapplication/MainActivity.kt
@@ -90,12 +90,14 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     private fun playGame(clickedView: Button, buttonNumber: Int) {
         if (activePlayer == 1) {
             clickedView.text = "X"
+            clickedView.setTextColor(Color.CYAN)
             clickedView.setBackgroundColor(Color.RED)
             firstPlayer.add(buttonNumber)
             activePlayer = 2
         } else {
             clickedView.text = "0"
             clickedView.setBackgroundColor(Color.YELLOW)
+            clickedView.setTextColor(Color.BLUE)
             secondPlayer.add(buttonNumber)
             activePlayer = 1
         }
@@ -108,77 +110,77 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
        if (firstPlayer.contains(1) && firstPlayer.contains(2) && firstPlayer.contains(3)) {
            winnerPlayer = 1
-           disableButtons()
+
 
        }
        if (secondPlayer.contains(1) && secondPlayer.contains(2) && secondPlayer.contains(3)) {
            winnerPlayer = 2
-           disableButtons()
+
        }
 
        if (firstPlayer.contains(4) && firstPlayer.contains(5) && firstPlayer.contains(6)) {
            winnerPlayer = 1
-           disableButtons()
+
        }
        if (secondPlayer.contains(4) && secondPlayer.contains(5) && secondPlayer.contains(6)) {
            winnerPlayer = 2
-           disableButtons()
+
        }
 
        if (firstPlayer.contains(7) && firstPlayer.contains(8) && firstPlayer.contains(9)) {
            winnerPlayer = 1
-           disableButtons()
+
        }
        if (secondPlayer.contains(7) && secondPlayer.contains(8) && secondPlayer.contains(9)) {
            winnerPlayer = 2
-           disableButtons()
+
        }
 
        if (firstPlayer.contains(1) && firstPlayer.contains(4) && firstPlayer.contains(7)) {
            winnerPlayer = 1
-           disableButtons()
+
        }
        if (secondPlayer.contains(1) && secondPlayer.contains(4) && secondPlayer.contains(7)) {
            winnerPlayer = 2
-           disableButtons()
+
        }
 
        if (firstPlayer.contains(2) && firstPlayer.contains(5) && firstPlayer.contains(8)) {
            winnerPlayer = 1
-           disableButtons()
+
        }
        if (secondPlayer.contains(2) && secondPlayer.contains(5) && secondPlayer.contains(8)) {
            winnerPlayer = 2
-           disableButtons()
+
        }
 
        if (firstPlayer.contains(3) && firstPlayer.contains(6) && firstPlayer.contains(9)) {
            winnerPlayer = 1
-           disableButtons()
+
        }
        if (secondPlayer.contains(3) && secondPlayer.contains(6) && secondPlayer.contains(9)) {
            winnerPlayer = 2
-           disableButtons()
+
        }
 
        if (firstPlayer.contains(3) && firstPlayer.contains(5) && firstPlayer.contains(7)) {
            winnerPlayer = 1
-           disableButtons()
+
        }
        if (secondPlayer.contains(3) && secondPlayer.contains(5) && secondPlayer.contains(7)) {
            winnerPlayer = 2
-           disableButtons()
+
        }
 
        if (firstPlayer.contains(1) && firstPlayer.contains(5) && firstPlayer.contains(9)) {
            winnerPlayer = 1
-           disableButtons()
+
        }
        if (secondPlayer.contains(1) && secondPlayer.contains(5) && secondPlayer.contains(9)) {
            winnerPlayer = 2
-           disableButtons()
+
        }
-       if ((winnerPlayer!=1)&&(winnerPlayer!=2) &&(button1.text!=""&& button2.text!=""&& button3.text!=""&& button4.text!=""&& button5.text!=""&& button6.text!=""&& button7.text!=""&& button8.text!=""&& button9.text!="")) {
+       if ((winnerPlayer!=1)&&(winnerPlayer!=2) &&((button1.isEnabled==false) && (button2.isEnabled==false) && (button3.isEnabled==false) && (button4.isEnabled==false) && (button5.isEnabled==false) && (button6.isEnabled==false) && (button7.isEnabled==false) && (button8.isEnabled==false) && (button9.isEnabled==false))){
                     winnerPlayer=3
 
            }
@@ -188,15 +190,18 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
         if (winnerPlayer != 0) {
             if (winnerPlayer == 1) {
+
                 Toast.makeText(this, "X wins!", Toast.LENGTH_LONG).show()
                 winnerPlayer=0
             }
             if (winnerPlayer==3){
-                Toast.makeText(this, "It is a draw!", Toast.LENGTH_LONG).show()
+                Toast.makeText(this, "Neither of you won!", Toast.LENGTH_LONG).show()
+
                 winnerPlayer=0
             }
 
             if (winnerPlayer==2) {
+
                 Toast.makeText(this, "0 wins!", Toast.LENGTH_LONG).show()
                 winnerPlayer=0
 
@@ -221,27 +226,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
 
     }
-
-    fun resetclick(clickedView: View)   {
-
-        button1.setBackgroundColor(Color.MAGENTA)
-        button1.text=""
-        button2.setBackgroundColor(Color.MAGENTA)
-        button2.text=""
-        button3.setBackgroundColor(Color.MAGENTA)
-        button3.text=""
-        button4.setBackgroundColor(Color.MAGENTA)
-        button4.text=""
-        button5.setBackgroundColor(Color.MAGENTA)
-        button5.text=""
-        button6.setBackgroundColor(Color.MAGENTA)
-        button6.text=""
-        button7.setBackgroundColor(Color.MAGENTA)
-        button7.text=""
-        button8.setBackgroundColor(Color.MAGENTA)
-        button8.text=""
-        button9.setBackgroundColor(MAGENTA)
-        button9.text=""
+    private fun enableButtons(){
         button1.isEnabled=true
         button2.isEnabled=true
         button3.isEnabled=true
@@ -251,9 +236,33 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         button7.isEnabled=true
         button8.isEnabled=true
         button9.isEnabled=true
+    }
+
+    fun resetClick(clickedView: View)   {
+        enableButtons()
+        button1.setBackgroundColor(Color.DKGRAY)
+        button2.setBackgroundColor(Color.DKGRAY)
+        button3.setBackgroundColor(Color.DKGRAY)
+        button4.setBackgroundColor(Color.DKGRAY)
+        button5.setBackgroundColor(Color.DKGRAY)
+        button6.setBackgroundColor(Color.DKGRAY)
+        button7.setBackgroundColor(Color.DKGRAY)
+        button8.setBackgroundColor(Color.DKGRAY)
+        button9.setBackgroundColor(Color.DKGRAY)
+        button1.text=" "
+        button2.text=" "
+        button3.text=" "
+        button4.text=" "
+        button5.text=" "
+        button6.text=" "
+        button7.text=" "
+        button8.text=" "
+        button9.text=" "
         activePlayer=1
-        firstPlayer.clear()
         secondPlayer.clear()
+        firstPlayer.clear()
+
+
 
 
 
